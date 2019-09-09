c2584b7219c7c2c00b9a09345a3a982171aec566
chenyn <1048196441@qq.com>
2019-09-09 14:35:19 +0800

    我的页面约束冲突问题

--- lib/Mine/View/MineListCell.dart
+++ lib/Mine/View/MineListCell.dart
@@ -7,7 +7,6 @@ import 'package:cajian/Base/CJUtils.dart';
 import 'package:cajian/Mine/Model/MineModel.dart';
 
 class MineListCellOthers extends StatelessWidget {
-
   final MineModel model;
   MineListCellOthers(this.model);
 
@@ -15,29 +14,47 @@ class MineListCellOthers extends StatelessWidget {
   Widget build(BuildContext context) {
     Size screenSize = getSize(context);
     return GestureDetector(
-        child: Container(
-          height: 48,
-          width: screenSize.width,
-          color: Colors.white,
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              new Padding(padding: EdgeInsets.symmetric(horizontal: 10),),
-              new Image.asset(model.icon),
-              new Padding(padding: EdgeInsets.symmetric(horizontal: 12),),
-              new Text(model.title, ),
-              new Padding(padding: EdgeInsets.symmetric(horizontal: 5),),
-              model.tipIcon != null ? new Image.asset(model.tipIcon) : Divider(height: 0),
-              Expanded(flex: 1, child: SizedBox(),),
-              new Icon(Icons.arrow_forward_ios, size: 16,),
-              Padding(padding: EdgeInsets.symmetric(horizontal: 6),),
-            ],
-          ),
+      child: Container(
+        height: 48,
+        width: screenSize.width,
+        color: Colors.white,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+            ),
+            new Image.asset(model.icon),
+            new Padding(
+              padding: EdgeInsets.symmetric(horizontal: 12),
+            ),
+            new Text(
+              model.title,
+            ),
+            new Padding(
+              padding: EdgeInsets.symmetric(horizontal: 5),
+            ),
+            model.tipIcon != null
+                ? new Image.asset(model.tipIcon)
+                : Divider(height: 0),
+            Expanded(
+              flex: 1,
+              child: SizedBox(),
+            ),
+            new Icon(
+              Icons.arrow_forward_ios,
+              size: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6),
+            ),
+          ],
         ),
-        onTap: (){
-          model.onTap(context);
-        },
+      ),
+      onTap: () {
+        model.onTap(context);
+      },
     );
   }
 }
@@ -53,8 +70,7 @@ class MineListCellSeparator extends StatelessWidget {
 }
 
 // 用户信息
-class MineListProfileHeader extends StatefulWidget 
-{
+class MineListProfileHeader extends StatefulWidget {
   final MineModel model;
   MineListProfileHeader(this.model);
 
@@ -63,9 +79,7 @@ class MineListProfileHeader extends StatefulWidget
   }
 }
 
-class MineListProfileState extends State<MineListProfileHeader>
-{
-
+class MineListProfileState extends State<MineListProfileHeader> {
   String _avatarUrl;
   String _showName;
   String _cajianNo;
@@ -80,47 +94,69 @@ class MineListProfileState extends State<MineListProfileHeader>
   fetchInfo() async {
     dynamic info = await widget.model.mineInfo();
     setState(() {
-        _avatarUrl = info['avatarUrl'];
-        _showName  = info['name'];
-        _cajianNo = info['cajian_no'];
+      _avatarUrl = info['avatarUrl'];
+      _showName = info['name'];
+      _cajianNo = info['cajian_no'];
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
-
     return Container(
-      height: 103,
-      color: Colors.white,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Padding(padding: EdgeInsets.symmetric(horizontal: 16),),
-          _avatarUrl != null?Image.network(_avatarUrl, width: 56)
-                  : Container(color: Colors.grey, width: 56, height: 56,),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 10),),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                _showName??'',
-                style: TextStyle(fontSize: 17, color: BlackColor),
-              ),
-              Text(
-                '擦肩号：$_cajianNo', 
-                style: TextStyle(fontSize: 14, color: Color(0xFF9B9B9B)),
-              )
-            ],
-          ),
-          Expanded(flex: 1, child: SizedBox(),),
-          Image.asset('images/icon_settings_gray_qr@2x.png', width: 14,),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 26),),
-          new Icon(Icons.arrow_forward_ios, size: 16,),
-          Padding(padding: EdgeInsets.symmetric(horizontal: 6),),
-        ],
-      )
-    );
+        height: 103,
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+            ),
+            _avatarUrl != null
+                ? Image.network(_avatarUrl, width: 56)
+                : Container(
+                    color: Colors.grey,
+                    width: 56,
+                    height: 56,
+                  ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+            ),
+            Expanded(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  _showName ?? '',
+                  style: TextStyle(fontSize: 17, color: BlackColor),
+                ),
+                Text(
+                  '擦肩号：$_cajianNo',
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Color(0xFF9B9B9B),
+                  ),
+                  overflow: TextOverflow.fade,
+                  softWrap: false,
+                ),
+              ],
+            )),
+            Image.asset(
+              'images/icon_settings_gray_qr@2x.png',
+              width: 14,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16),
+            ),
+            new Icon(
+              Icons.arrow_forward_ios,
+              size: 16,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 6),
+            ),
+          ],
+        ));
   }
 }
