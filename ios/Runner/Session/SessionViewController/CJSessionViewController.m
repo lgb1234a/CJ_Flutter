920e8dc84ac4808df9ddcea5dc4426e0bf972724
chenyn <1048196441@qq.com>
2019-09-20 17:32:12 +0800

    事件入口整理

--- ios/Runner/Session/SessionViewController/CJSessionViewController.m
+++ ios/Runner/Session/SessionViewController/CJSessionViewController.m
@@ -21,7 +21,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    
+    /* 配置导航条按钮 */
+    [self setUpNavBarItem];
 }
 
 /* 重新修改session配置 */
@@ -34,55 +35,79 @@
     return _sessionConfig;
 }
 
+- (void)setUpNavBarItem
+{
+    UIButton *enterTeamCard = [UIButton buttonWithType:UIButtonTypeCustom];
+    [enterTeamCard addTarget:self action:@selector(enterSessionInfoPage:) forControlEvents:UIControlEventTouchUpInside];
+    [enterTeamCard setImage:[UIImage imageNamed:@"icon_session_info_normal"] forState:UIControlStateNormal];
+    [enterTeamCard setImage:[UIImage imageNamed:@"icon_session_info_pressed"] forState:UIControlStateHighlighted];
+    [enterTeamCard sizeToFit];
+    
+    UIBarButtonItem *enterTeamCardItem = [[UIBarButtonItem alloc] initWithCustomView:enterTeamCard];
+    
+    if (self.session.sessionType == NIMSessionTypeTeam)
+    {
+        self.navigationItem.rightBarButtonItems  = @[enterTeamCardItem];
+    }
+}
+
 
 #pragma mark - NIMMeidaButton
 - (void)onTapMediaItemCajianRP:(NIMMediaItem *)item
 {
+    // TODO: 擦肩红包
     
 }
 
 - (void)onTapMediaItemCloudRedPacket:(NIMMediaItem *)item
 {
-    
+    // TODO:云红包
 }
 
 - (void)onTapMediaItemYeePacket:(NIMMediaItem *)item
 {
-    
+    // TODO:易红包
 }
 
 - (void)onTapMediaItemYXTransfer:(NIMMediaItem *)item
 {
-    
+    // TODO:易转账
 }
 
 - (void)onTapMediaItemProfileCard:(NIMMediaItem *)item
 {
-    
+    // TODO:名片
 }
 
 - (void)onTapMediaItemAliPayCode:(NIMMediaItem *)item
 {
-    
+    // TODO:收款码
 }
 
 - (void)onTapMediaItemPersonalstamp:(NIMMediaItem *)item
 {
-    
+    // TODO:抖一抖
 }
 
 - (void)onTapMediaItemTeamNotice:(NIMMediaItem *)item
 {
-    
+    // TODO:群通知
 }
 
 - (void)onTapMediaItemCollection:(NIMMediaItem *)item
 {
-    
+    // TODO:发收藏
 }
 
 - (void)onTapMediaItemLocation:(NIMMediaItem *)item
 {
+    // TODO:发定位
+}
+
+#pragma mark - private
+- (void)enterSessionInfoPage:(id)sender
+{
+    // TODO:跳转flutter 聊天信息页
     
 }
 
