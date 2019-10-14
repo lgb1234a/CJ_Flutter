def16ad3b85976990f7ab281d3aff3e7a10241da
chenyn <1048196441@qq.com>
2019-10-14 17:45:18 +0800

    搜索列表

--- lib/Contacts/ContactsSearching.dart
+++ lib/Contacts/ContactsSearching.dart
@@ -1,4 +1,3 @@
-
 /**
  *  Created by chenyn on 2019-10-11
  *  通讯录搜索页
@@ -10,6 +9,7 @@ import 'package:cajian/Base/CJUtils.dart';
 import 'Model/ContactSearchDataSource.dart';
 import 'package:nim_sdk_util/nim_contactModel.dart';
 import 'package:nim_sdk_util/nim_teamModel.dart';
+import 'package:nim_sdk_util/nim_searchInterface.dart';
 
 class ContactsSearchingWidget extends StatefulWidget {
   final Function cancel;
@@ -24,8 +24,8 @@ class ContactsSearchingWidget extends StatefulWidget {
 class ContactsSearchingState extends State<ContactsSearchingWidget> {
   int _searchBarHeight = 50;
   TextEditingController _searchController = TextEditingController();
-  List<ContactInfo> _contacts;
-  List<TeamInfo> _teams;
+  List<ContactInfo> _contacts = [];
+  List<TeamInfo> _teams = [];
 
   @override
   void initState() {
@@ -43,8 +43,10 @@ class ContactsSearchingState extends State<ContactsSearchingWidget> {
 
   // 搜索文本变化
   void searchTextChanged() async {
-    List contacts = await ContactSearchDataSource.searchContactBy(_searchController.text);
-    List groups = await ContactSearchDataSource.searchGroupBy(_searchController.text);
+    List<ContactInfo> contacts =
+        await ContactSearchDataSource.searchContactBy(_searchController.text);
+    List<TeamInfo> groups =
+        await ContactSearchDataSource.searchGroupBy(_searchController.text);
     setState(() {
       _contacts = contacts;
       _teams = groups;
@@ -97,10 +99,136 @@ class ContactsSearchingState extends State<ContactsSearchingWidget> {
         ));
   }
 
+  // cell
+  Widget _buildItem(BuildContext context, CJSearchInterface model) {
+    Widget avatar = Container(color: Colors.grey, width: 44, height: 44);
+    double itemHeight = 72.1;
+    if (model is ContactInfo) {
+      return SizedBox(
+        height: itemHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Divider(
+              height: 0.1,
+              indent: 12,
+            ),
+            ListTile(
+              leading: model.avatarUrlString != null
+                  ? Image.network(model.avatarUrlString, width: 44, height: 44)
+                  : avatar,
+              title: Text(model.showName),
+              subtitle: Text(''), // 显示匹配的关键词
+              onTap: () {
+                // 点击跳转到个人信息
+              },
+            )
+          ],
+        ),
+      );
+    }
+
+    if (model is TeamInfo) {
+      return SizedBox(
+        height: itemHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Divider(
+              height: 0.1,
+              indent: 12,
+            ),
+            ListTile(
+              leading: model.teamAvatar != null
+                  ? Image.network(model.teamAvatar, width: 44, height: 44)
+                  : avatar,
+              title: Text(model.teamName),
+              subtitle: Text(''), // 显示匹配的关键词
+              onTap: () {
+                // 点击跳转到群聊
+              },
+            )
+          ],
+        ),
+      );
+    }
+
+    return Center(
+      child: Text('未匹配到相关数据类型！'),
+    );
+  }
+
+  Widget _buildSection(BuildContext context, int index) {
+    double screenWidth = getSize(context).width;
+    List<Widget> contacts =
+        _contacts.map((f) => _buildItem(context, f)).toList();
+    List<Widget> teams = _teams.map((f) => _buildItem(context, f)).toList();
+
+    contacts.insert(
+        0,
+        Container(
+          height: 30,
+          width: screenWidth,
+          padding: EdgeInsetsDirectional.only(start: 12),
+          child: Text('联系人'),
+        ));
+    teams.insert(
+        0,
+        Container(
+          height: 30,
+          width: screenWidth,
+          padding: EdgeInsetsDirectional.only(start: 12),
+          child: Text('群聊'),
+        ));
+    // 先联系人，再群聊
+    if (index == 0) {
+      if (_contacts.length > 0) {
+        return Column(
+          children: contacts,
+        );
+      } else if (_teams.length > 0) {
+        return Column(
+          children: teams,
+        );
+      }
+    } else if (index == 1 && _teams.length > 0) {
+      return Column(
+        children: teams,
+      );
+    }
+
+    return Center(
+      child: Text('未搜索到相关信息~'),
+    );
+  }
+
+  // search List
+  Widget _searchList(BuildContext context) {
+    int itemCount = _contacts.length > 0 && _teams.length > 0 ? 2 : 1;
+    return ListView.separated(
+      itemCount: itemCount,
+      itemBuilder: (context, idx) => _buildSection(context, idx),
+      separatorBuilder: (context, idx) {
+        return Container(
+          height: 9,
+          color: Color(0xffe5e5e5),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _buildSearchBar(context),
+      body: Column(
+        children: <Widget>[
+          _buildSearchBar(context),
+          Expanded(
+            flex: 1,
+            child: _searchList(context),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/Contacts/Model/ContactSearchDataSource.dart
+++ lib/Contacts/Model/ContactSearchDataSource.dart
@@ -3,21 +3,25 @@
  *  通讯录搜索数据源
  */
 import 'package:nim_sdk_util/nim_sdk_util.dart';
-import 'package:nim_sdk_util/nim_searchInterface.dart';
 import 'package:nim_sdk_util/nim_contactModel.dart';
 import 'package:nim_sdk_util/nim_teamModel.dart';
 
 class ContactSearchDataSource {
   static bool isKeyContained(String key, String origin) {
+    if(origin == null || key == null) {
+      return false;
+    }
+    if(key.length == 0) {
+      return false;
+    }
     return origin.contains(key);
   }
 
   // 查找联系人
-  static Future<List<CJSearchInterface>> searchContactBy(String key) async {
+  static Future<List<ContactInfo>> searchContactBy(String key) async {
     List<ContactInfo> friends = await NimSdkUtil.friends();
     List<ContactInfo> result = [];
     friends.forEach((e) {
-
       if (isKeyContained(key, e.showName) ||
           isKeyContained(key, e.infoId)) {
         e.keyword = key;
@@ -28,11 +32,10 @@ class ContactSearchDataSource {
   }
 
   // 查找群聊
-  static Future<List<CJSearchInterface>> searchGroupBy(String key) async {
+  static Future<List<TeamInfo>> searchGroupBy(String key) async {
     List<TeamInfo> teams = await NimSdkUtil.allMyTeams();
     List<TeamInfo> result = [];
     teams.forEach((t) async {
-
       if(isKeyContained(key, t.teamName) || isKeyContained(key, t.teamId)){
         t.keyword = key;
         result.add(t);
