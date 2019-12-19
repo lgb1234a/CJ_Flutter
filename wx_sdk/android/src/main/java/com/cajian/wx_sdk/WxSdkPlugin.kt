93a05c70317b5b3d86b38b26b997bf356576b463
城 彭 <pengcheng@tonnn.com>
2019-12-19 11:26:24 +0800

    Android微信Flutter插件同步iOS

--- wx_sdk/android/src/main/java/com/cajian/wx_sdk/WxSdkPlugin.kt
+++ wx_sdk/android/src/main/java/com/cajian/wx_sdk/WxSdkPlugin.kt
@@ -15,6 +15,8 @@ import com.netease.nimlib.sdk.auth.LoginInfo
 import com.tencent.mm.opensdk.modelbase.BaseResp
 import com.tencent.mm.opensdk.modelmsg.SendAuth
 import com.tencent.mm.opensdk.modelmsg.SendMessageToWX
+import com.tencent.mm.opensdk.modelmsg.WXMediaMessage
+import com.tencent.mm.opensdk.modelmsg.WXWebpageObject
 import com.tencent.mm.opensdk.openapi.IWXAPI
 import com.tencent.mm.opensdk.openapi.WXAPIFactory
 import com.youxi.chat.base.Config
@@ -78,11 +80,42 @@ class WxSdkPlugin private constructor(private val mRegistrar: Registrar) : Metho
         result.success(null)
     }
 
+    /**
+     * 微信分享
+     */
+    private fun share(params: Map<*, *>, result: MethodChannel.Result) {
+        val title = params["title"] as String?
+        val content = params["content"] as String
+        val url = params["url"] as String
+        val type = params["type"] as Int
+
+        val message = WXMediaMessage()
+        message.title = title ?: ""
+        message.description = content
+        message.messageExt = content
+        message.messageAction = content
+
+        val ext = WXWebpageObject()
+        ext.webpageUrl = url
+
+        message.mediaObject = ext
+
+        val req = SendMessageToWX.Req()
+        req.transaction = buildTransaction("text")
+        req.message = message
+        req.scene = SendMessageToWX.Req.WXSceneSession
+
+        mWxApi.sendReq(req)
+    }
+
     private fun onResp(_resp: BaseResp) {
         if (_resp is SendMessageToWX.Resp) {
-            if (_resp.errCode == 0) { // 什么也不做
+            // 分享成功是否的监听
+            if (_resp.errCode == 0) {
+                // 什么也不做
             }
         } else if (_resp is SendAuth.Resp) {
+            // 授权结果监听
             if (_resp.errCode == 0) {
                 val resp = _resp
                 val accessToken = resp.code
@@ -137,7 +170,7 @@ class WxSdkPlugin private constructor(private val mRegistrar: Registrar) : Metho
                 "accid" to accid,
                 "code" to code,
                 "union_id" to "",
-                "app_key" to "wx0f56e7c5e6daa01a"
+                "app_key" to APP_ID
         )
 
         CommonApi.getApi().postJson(Config.wechatBindUrl, fieldMap = params)
@@ -173,7 +206,7 @@ class WxSdkPlugin private constructor(private val mRegistrar: Registrar) : Metho
 
         val params = mapOf(
                 "code" to accessToken,
-                "app_key" to "wx0f56e7c5e6daa01a"
+                "app_key" to APP_ID
         )
 
         CommonApi.getApi().postJson(Config.wechatLoginUrl, fieldMap = params)
@@ -199,6 +232,88 @@ class WxSdkPlugin private constructor(private val mRegistrar: Registrar) : Metho
                         ToastUtils.showShort(e.message)
                     }
                 })
+    }
+
+    /**
+     * 查询微信绑定状态
+     */
+    private fun wxBindStatus(params: Map<*, *>, result: MethodChannel.Result) {
+        val accid = NimUIKit.getAccount()
+
+        val params = mapOf(
+                "accid" to accid,
+                "app_key" to APP_ID
+        )
+
+        CommonApi.getApi().postJson(Config.wechatStatusUrl, fieldMap = params)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<String> {
+                    override fun onComplete() {
+                        PopupManager.hideLoading(mContext)
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                        PopupManager.showLoading(mContext)
+                    }
+
+                    override fun onNext(t: String) {
+                        val model = GsonBuilder().setLenient().create().fromJson(t, BaseModel::class.java)
+                        if (model.success()) {
+                            result.success(true)
+                        } else {
+                            result.success(false)
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+                        PopupManager.hideLoading(mContext)
+                        result.success(false)
+                    }
+                })
+    }
+
+    /**
+     * 解绑微信
+     */
+    private fun unBindWeChat(params: Map<*, *>, result: MethodChannel.Result) {
+        val accid = NimUIKit.getAccount()
+
+        val params = mapOf(
+                "accid" to accid,
+                "app_key" to APP_ID
+        )
+
+        CommonApi.getApi().postJson(Config.wechatUnbindUrl, fieldMap = params)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<String> {
+                    override fun onComplete() {
+                        PopupManager.hideLoading(mContext)
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+                        PopupManager.showLoading(mContext, loadingTips = "登录中...")
+                    }
+
+                    override fun onNext(t: String) {
+                        val model = GsonBuilder().setLenient().create().fromJson(t, BaseModel::class.java)
+                        if (model.success()) {
+                            ToastUtils.showShort("解绑成功")
+                        } else {
+                            ToastUtils.showShort(model.errmsg)
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+                        PopupManager.hideLoading(mContext)
+                        ToastUtils.showShort(e.message)
+                    }
+                })
+    }
+
+    private fun buildTransaction(type: String?): String {
+        return if (type == null) System.currentTimeMillis().toString() else type + System.currentTimeMillis();
     }
 
     companion object {
