5ef57712d41f43bcada569b8e27474525bab63f0
chenyn <1048196441@qq.com>
2019-12-31 11:13:05 +0800

    会话列表页新增更多按钮-扫一扫功能

--- ios/Runner/Base/CJTabbarController.m
+++ ios/Runner/Base/CJTabbarController.m
@@ -10,6 +10,7 @@
 #import "CJMineViewController.h"
 #import "CJContactsViewController.h"
 #import "CJSessionListViewController.h"
+#import <CJPopOverMenuView.h>
 
 @interface CJTabbarController ()
 
@@ -20,6 +21,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.navigationItem.title = @"擦肩";
+    
+    UIButton *add = [UIButton buttonWithType:UIButtonTypeCustom];
+    [add setImage:[UIImage imageNamed:@"icon_group_add"]
+         forState:UIControlStateNormal];
+    
+    UIBarButtonItem *item = [[UIBarButtonItem alloc] initWithCustomView:add];
+    self.navigationItem.rightBarButtonItem = item;
+    
+    [add addTarget:self
+            action:@selector(showMore:)
+  forControlEvents:UIControlEventTouchUpInside];
 }
 
 - (instancetype)initWithRootViewControllers
@@ -49,5 +61,50 @@
     return self;
     
 }
+
+
+/// 选中更多
+- (void)showMore:(UIButton *)sender
+{
+    CGRect rct = [sender convertRect:sender.bounds toView:nil];
+    CGPoint startPoint = CGPointMake(rct.origin.x + rct.size.width*0.5,
+                                     rct.origin.y + STATUS_BAR_HEIGHT);
+    
+    CJPopOverMenuView *menuView = [CJPopOverMenuView popover];
+    menuView.didShowHandler = ^{
+        self.navigationItem.rightBarButtonItem.enabled = NO;
+    };
+    
+    menuView.didDismissHandler = ^{
+        self.navigationItem.rightBarButtonItem.enabled = YES;
+    };
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setTitle:@"扫一扫" forState:UIControlStateNormal];
+    [btn setTitleColor:Main_TextBlackColor forState:UIControlStateNormal];
+    btn.frame = CGRectMake(0, 0, 80, 30);
+    [btn addTarget:self
+            action:@selector(showScanView:)
+  forControlEvents:UIControlEventTouchUpInside];
+    
+    [menuView showAtPoint:startPoint
+           popoverPostion:CJPopoverPositionDown
+          withContentView:btn
+                   inView:self.view];
+}
+
+
+- (void)showScanView:(id)sender
+{
+    /// 打开扫一扫页面
+    [FlutterBoostPlugin open:@"nativePage://android&iosPageName=CJScanViewController"
+                   urlParams:@{}
+                        exts:@{@"animated": @(YES)}
+              onPageFinished:^(NSDictionary * _Nonnull d) {
+        
+    } completion:^(BOOL c) {
+        
+    }];
+}
+
 
 @end
